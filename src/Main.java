0afdfecf4a58552d8db6fc7aa0f12ea314026f6e
Kathrin Prielinger <k.prielinger@gmail.com>
2021-02-17 23:54:25 +0100

    add changes

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int i = 1;
+        int j = 100;
 
-        while (i <= 100) {
-            if  (((i % 3) == 0) & ((i % 5)) == 0) {
+        while (i <= j) {
+            if  (((i % 3) == 0) && ((i % 5)) == 0) {
                 System.out.println("FizzBuzz");
             } else if ((i % 5) == 0) {
                 System.out.println("Buzz");
